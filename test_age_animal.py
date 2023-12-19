530c1ee509bbac52697c9d2c91be17a759d72ab5
QUENTIN.GHR <quentin.ghr@eduge.ch>
2023-12-19 08:27:41 +0100

    Premier test

--- test_age_animal.py
+++ test_age_animal.py
@@ -1,9 +1,14 @@
 import unittest
+from datetime import datetime
+from Animal import Animal
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_est_un_animal(self):
+        date_str = "2023-12-19"
+        date_format = "%Y-%m-%d"
+        ma_date = datetime.strptime(date_str, date_format)
+        self.assertEqual("Je suis un animal du nom de Marley", Animal(ma_date, 'Marley'))  # add assertion here
 
 
 if __name__ == '__main__':
