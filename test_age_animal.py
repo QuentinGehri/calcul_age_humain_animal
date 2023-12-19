748d1b07c0508d1aeb67966e637c6ea44003db88
QUENTIN.GHR <quentin.ghr@eduge.ch>
2023-12-19 09:44:13 +0100

    Ecriture de la methode de test pour un chien de 2 ans

--- test_age_animal.py
+++ test_age_animal.py
@@ -43,12 +43,19 @@ class MyTestCase(unittest.TestCase):
         marley = Chien(ma_date, 'Marley')
         self.assertEqual("Je suis un chien du nom de Marley, né le 19.12.2023", marley.__str__())
 
-    def test_age_humain_1an_age_chien_15an_fonctionnel(self):
+    def test_age_humain_1an_age_chien_15ans_fonctionnel(self):
         date_str = "10.05.2022"
         date_format = "%d.%m.%Y"
         ma_date = datetime.strptime(date_str, date_format)
         marley = Chien(ma_date, 'Marley')
         self.assertEqual(15, marley.age_humain())
+
+    def test_age_humain_2ans_age_chien_24ans_fonctionnel(self):
+        date_str = "10.05.2021"
+        date_format = "%d.%m.%Y"
+        ma_date = datetime.strptime(date_str, date_format)
+        marley = Chien(ma_date, 'Marley')
+        self.assertEqual(24, marley.age_humain())
 
     # Chat
     def test_str_chat_fonctionnel(self):
