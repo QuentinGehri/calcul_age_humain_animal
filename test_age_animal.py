f380dceed6459ad54fd4c59ac9ffd33fa7919c7e
QUENTIN.GHR <quentin.ghr@eduge.ch>
2023-12-19 08:51:31 +0100

    Refactor du format de la date passée en paramètre

--- test_age_animal.py
+++ test_age_animal.py
@@ -6,15 +6,15 @@ from Chien import Chien
 
 class MyTestCase(unittest.TestCase):
     def test_str_animal_fonctionnel(self):
-        date_str = "19-12-2023"
-        date_format = "%d-%m-%Y"
+        date_str = "19.12.2023"
+        date_format = "%d.%m.%Y"
         ma_date = datetime.strptime(date_str, date_format)
         marley = Animal(ma_date, 'Marley')
         self.assertEqual("Je suis un animal du nom de Marley, né le 19.12.2023", marley.__str__())
 
     def test_str_chien_fonctionnel(self):
-        date_str = "19-12-2023"
-        date_format = "%d-%m-%Y"
+        date_str = "19.12.2023"
+        date_format = "%d.%m.%Y"
         ma_date = datetime.strptime(date_str, date_format)
         marley = Chien(ma_date, 'Marley')
         self.assertEqual("Je suis un chien du nom de Marley, né le 19.12.2023", marley.__str__())
