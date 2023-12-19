63cefd77342e38c72f1fc91f596272aa0c38e15b
QUENTIN.GHR <quentin.ghr@eduge.ch>
2023-12-19 09:43:09 +0100

    Refactor de la methode age_humain pour la classe Chien, si le chien a 1 an

--- Chien.py
+++ Chien.py
@@ -2,11 +2,18 @@ from Animal import Animal
 
 
 class Chien(Animal):
+    AGE_1AN = 15
+    AGE_2AN = 24
+    AJOUT_AGE_APRES_2AN = 5
+    AGE_APRES_LEQUEL_ON_AJOUTE = 2
+
     def __init__(self, date_anniv, nom):
         super().__init__(date_anniv, nom)
 
     def age_humain(self):
-        pass
+        age = self.calculer_age()
+        if age == 1:
+            return self.AGE_1AN
 
     def __str__(self):
         return super().__str__("chien")
